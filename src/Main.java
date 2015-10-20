7e2eac0c9a1f8bed8393d092040766ee3d6d4d53
Granger Lang <grangerlang8@hotmail.com>
2015-10-20 14:47:53 -0700

    first prototype (not working quite yet, but close, I think) - Granger

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the file name of the image to search within: ");
+        String largeImgName = scanner.nextLine();
+        System.out.print("Please enter the file name of the image to search for: ");
+        String smallImgName = scanner.nextLine();
+
+        if (containsImage(largeImgName, smallImgName))
+            System.out.print("The smaller image was found in the larger image.");
+        else
+            System.out.print("The smaller image was not found in the larger image.");
+    }
+
+    public static boolean containsImage(String largeImgName, String smallImgName) {
+        BufferedImage largeImg = null;
+        BufferedImage smallImg = null;
+
+        try {
+            largeImg = ImageIO.read(new File(largeImgName));
+            smallImg = ImageIO.read(new File(smallImgName));
+        } catch (IOException e) {
+            System.out.println("IOException Occurred");
+        }
+
+        for (int i = 0; i < largeImg.getWidth() - smallImg.getWidth(); i++) {
+            for (int j = 0; j < largeImg.getHeight() - largeImg.getHeight(); j++) {
+                if (imageEquals(smallImg, largeImg.getSubimage(i, j, smallImg.getWidth(), smallImg.getHeight())))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean imageEquals(BufferedImage image1, BufferedImage image2) {
+        for (int y = 0; y < image1.getHeight(); y++) {
+            for (int x = 0; x < image1.getWidth(); x++) {
+                if (image1.getRGB(x, y) != image2.getRGB(x, y)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
